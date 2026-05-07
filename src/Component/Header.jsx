06500af1cfacd7457d3d7986373501610b0f0efa
That guy <120946193+ezedike-evan@users.noreply.github.com>
2026-05-07 21:15:00 +0100

    Update image paths in Header component

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -5,11 +5,15 @@ const Header = () => {
     <header className='headerContainer'>
         <div className='headerHolder'>
             <div className='glovoLogo'>
-                <img src="src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/28a67897-50c3-452e-8c24-e9d7f910dd41.svg" alt="logo" />
+                <img src="../assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/28a67897-50c3-452e-8c24-e9d7f910dd41.svg" alt="logo" />
             </div>
 
             <div className='loginHolder'>
-                <span><button><img src="src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/a1b3cc6e-0cfa-470d-8d0f-a2157c4d17e9.svg" alt="" className='logoImg' /> Login</button></span>
+                <span>
+                  <button>
+                    <img src="../assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/a1b3cc6e-0cfa-470d-8d0f-a2157c4d17e9.svg" alt="" className='logoImg' /> Login
+                  </button>
+                </span>
             </div>
  
         </div>
