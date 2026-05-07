93c0e2e2f51a477c879422187b121b9e018efd1e
Maryjane Nmesoma <nmesomman@gmail.com>
2026-05-07 17:11:36 +0100

    style: completed the hero section mobile responsive design

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -10,9 +10,7 @@ const Hero = () => {
         </div>
         <div className='inputHolder'>
           <div className='firstinputHolder'>   
-            <p className='firstInput'>
-              Food delivery and 
-            </p>
+            <p className='firstInput'>Food delivery and </p>
             <p>more</p>
             <p className='secondInput'>Goceries, shops, pharmercies, anything</p>
           </div>
@@ -27,33 +25,3 @@ const Hero = () => {
 }
 
 export default Hero
-
-
-
-
-
-
-
-
-
-
-
-// mock.js
-// export const top3fg= [
-//     {
-//         resurantImg: "/asset/sivfnf.png",
-//         resutdgname: "the Place"
-//     },
-//     {}
-// ]
-
-// //topRe.jsx
-// import toplsdv from "mock.js"
-// top3fg.map((resurant, index)=>(
-//     // <resurantCard resurant={resurant} key={index}/>
-//     <div key={index} style={{position:"relative"}}>
-//         <img src={resurant.resurantImg} sty alt="" />
-//         {/* <p style={{marginTop:"-10px"}}>{resurant.resutdgname}</p> */}
-//         <p style={{position:"absolute", bottom:0}}>{resurant.resutdgname}</p>
-//     </div>
-// ))
