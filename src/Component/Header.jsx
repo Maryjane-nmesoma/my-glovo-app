e90a3b43ca160baee87e4dfde34740c0c6fb63f9
Ezedike-Evan <egwomevan323@gmail.com>
2026-05-07 21:31:48 +0100

    refactor: rename Header CSS file, simplify header structure, and remove unused React imports

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,21 +1,14 @@
-import React from 'react'
-import "../Component/CSS/Headder.css"
+import "../Component/CSS/Header.css"
 const Header = () => {
   return (
     <header className='headerContainer'>
         <div className='headerHolder'>
-            <div className='glovoLogo'>
-                <img src="/logo.svg" alt="logo" />
-            </div>
-
-            <div className='loginHolder'>
-                <span>
-                  <button>
-                    <img src="/user.svg" alt="" className='logoImg' /> Login
-                  </button>
-                </span>
-            </div>
- 
+          <div className='glovoLogo'>
+            <img src="/logo.svg" alt="logo" />
+          </div>
+          <button className='loginButton'>
+            <img src="/user.svg" alt="" className='logoImg' /> Login
+          </button>
         </div>
     </header>
   )
